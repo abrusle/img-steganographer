2e02ab325e74368f276c662264468f7dd77797a4
abrusle <15620365+abrusle@users.noreply.github.com>
2023-01-22 15:57:35 +0100

    Command parsing + split into modules

---
+++ src/cli.rs
@@ -0,0 +1,70 @@
+// Command-Line Interface
+
+pub enum Command
+{
+    Help,
+    Encode(bool, String, String),
+    Decode(String)
+}
+
+pub fn start() -> Result<Command, String>
+{
+    let args: Vec<String> = std::env::args().collect();
+
+    let cmd_string = args.get(1);
+
+    if cmd_string.is_none() {
+        return Result::Ok(Command::Help)
+    }
+
+    let cmd_string = cmd_string.unwrap().as_str();
+
+    return match cmd_string {
+        "help" => Result::Ok(Command::Help),
+
+        "encode" => {
+            let arg1 = args.get(2);
+            if arg1.is_none() {
+                Result::Err(String::from("Missing required arguments for message and file path."))
+            } else {
+                let repeat_or_msg = arg1.unwrap();
+                let repeat = repeat_or_msg == "-r" || repeat_or_msg == "--repeat";
+                let message = if repeat {args.get(3)} else {Some(repeat_or_msg)};
+                if message.is_none() {
+                    Result::Err(String::from("Missing required argument for message."))
+                } else {
+                    let file_path = args.get(if repeat {4} else {3});
+                    if file_path.is_none() {
+                        Result::Err(String::from("Missing required argument for file path."))
+                    } else {
+                        Result::Ok(Command::Encode(repeat, message.unwrap().to_owned(), file_path.unwrap().to_owned()))
+                    }
+                }
+            }
+        },
+
+        "decode" => {
+            match args.get(2) {
+                None => Result::Err(String::from("Missing required argument for file path.")),
+                Some(file_path) => Result::Ok(Command::Decode(file_path.to_owned()))
+            }
+        },
+
+        c => {
+            let mut err_msg = String::from("Unkown command ");
+            err_msg.push_str(&c);
+            Result::Err(err_msg)
+        }
+    };
+}
+
+pub fn print_help()
+{
+    println!("\
+Available commands:\n\
+\t help                                          Shows the help message.\n\
+\t encode [-r | --repeat] <message> <file-path>  Encodes the given message into the image.\n\
+\t decode <file-path>                            Decodes a message from the image.\n\
+\n\
+Supported image formats: .png");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,25 @@
 
+mod engine;
+mod cli;
+
+use cli::Command;
 
 fn main() {
-    let all_commands = ["help", "encode", "decode"];
-    let args: Vec<String> = std::env::args().collect();
 
-    let options = args.get(1);
-
-    let command = match options {
-        None => {
-            print_help();
+    match cli::start() {
+        Ok(cmd) => run_command(&cmd),
+        Err(msg) => {
+            println!("{}", msg);
             return;
-        },
-        Some(cmd) => {
-            if cmd == "help" {
-                print_help();
-                return;
-            }
-
-            if !all_commands.contains(&cmd.as_str()) {
-                println!("Unknown command {}", cmd);
-                return;
-            }
-
-            cmd
         }
+    }
+}
+
+fn run_command(cmd: &cli::Command) {
+    match cmd {
+        Command::Help => cli::print_help(),
+        _ => return // TODO
     };
-
-    print!("running command {}", command);
 }
 
-fn print_help()
-{
-    println!("\
-Available commands:\n\
-\t help                                          Shows the help message.\n\
-\t encode [-r | --repeat] <message> <file-path>  Encodes the given message into the image.\n\
-\t decode <file-path>                            Decodes a message from the image.\n\
-\n\
-Supported image formats: .png");
-}
+
